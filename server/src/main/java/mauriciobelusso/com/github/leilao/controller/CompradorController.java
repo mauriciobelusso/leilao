2a4ef1881a3501b68e803b23b888742289ef5ed9
mauriciobelusso <mauriciobelusso@gmail.com>
2023-01-15 08:43:00 -0300

    Tratado para o CompradorController fazer o GET adequadamente usando EmbbededId

--- server/src/main/java/mauriciobelusso/com/github/leilao/controller/CompradorController.java
+++ server/src/main/java/mauriciobelusso/com/github/leilao/controller/CompradorController.java
@@ -4,6 +4,9 @@ import mauriciobelusso.com.github.leilao.entity.Comprador;
 import mauriciobelusso.com.github.leilao.entity.CompradorId;
 import mauriciobelusso.com.github.leilao.service.CrudService;
 import mauriciobelusso.com.github.leilao.service.CompradorService;
+import net.minidev.json.annotate.JsonIgnore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,16 @@ public class CompradorController extends CrudController<Comprador, CompradorId>{
     @Override
     protected CrudService<Comprador, CompradorId> getService() {
         return compradorService;
+    }
+
+    @GetMapping("/{empresa}/{leilao}")
+    public Comprador findOne(@PathVariable Integer empresa, @PathVariable Integer leilao) {
+        return getService().findOne(new CompradorId(empresa, leilao));
+    }
+
+    @Override
+    @JsonIgnore
+    public Comprador findOne(CompradorId compradorId) {
+        return super.findOne(compradorId);
     }
 }
